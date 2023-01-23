861ee15b4e1d3d601b50e38a9276c2e885a61dad
Frenchfan <sumkin.gpb@gmail.com>
2023-01-23 23:34:48 +0300

    creating the first Bean via Spring

---
+++ src/test/java/ru/sumkin/spring_course/SpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sumkin.spring_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
