402b7e2941f066114c10f4a3f4ff51c9f979b94d
Frenchfan <sumkin.gpb@gmail.com>
2023-02-02 00:56:27 +0300

    Video14 - @Autowired (конструктор), @Component(перед классом)

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -1,19 +1,24 @@
 package spring_introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
     private Pet pet;
     private String surname;
     private int age;
 
-//    public Person(Pet pet) {
-//        System.out.println("Person bean is created");
-//        this.pet = pet;
-//    }
+    @Autowired
+    public Person(Pet pet) {
+        System.out.println("Person bean is created");
+        this.pet = pet;
+    }
 
     //pet -> setPet
-    public Person() {
-        System.out.println("Person bean is created");
-    }
+//    public Person() {
+//        System.out.println("Person bean is created");
+//    }
 
     public void setPet(Pet pet) {
         System.out.println("Class Person: set pet");
