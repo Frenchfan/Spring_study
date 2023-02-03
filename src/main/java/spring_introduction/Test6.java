293609a2e87dd07c50f4292558b9a0b241bb8b7d
Frenchfan <sumkin.gpb@gmail.com>
2023-02-04 01:42:46 +0300

    Video22 - @PropertySource

--- src/main/java/spring_introduction/Test6.java
+++ src/main/java/spring_introduction/Test6.java
@@ -13,7 +13,9 @@ public class Test6 {
 
 //        cat.say();
         Person person = context.getBean("personBean", Person.class);
-        person.callYourPet();
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
+//        person.callYourPet();
         context.close();
     }
 }
