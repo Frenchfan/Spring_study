ee4ea39aa618a6157969580d3e68cc89d9e563e2
Frenchfan <sumkin.gpb@gmail.com>
2023-02-05 21:58:18 +0300

    Video5 AOP - Pointcut expression, Declaration

--- src/main/java/aop/Test1.java
+++ src/main/java/aop/Test1.java
@@ -8,11 +8,11 @@ public class Test1 {
                 new AnnotationConfigApplicationContext(MyConfig.class);
 
         UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
-        Book book = context.getBean("book", Book.class);
+//        Book book = context.getBean("book", Book.class);
 
-        uniLibrary.getBook(book);
+        uniLibrary.getBook();
 //        uniLibrary.returnBook();
-//        uniLibrary.getMagazine();
+        uniLibrary.getMagazine();
 
 //        SchoolLibrary schoolLibrary =
 //                context.getBean("schoolLibrary", SchoolLibrary.class);
