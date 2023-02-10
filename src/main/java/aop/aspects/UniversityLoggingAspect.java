84a5b4cbde05e6bec23821178cb429f116cf94de
Frenchfan <sumkin.gpb@gmail.com>
2023-02-10 13:32:19 +0300

    Video11 AOP - @After Advice

--- src/main/java/aop/aspects/UniversityLoggingAspect.java
+++ src/main/java/aop/aspects/UniversityLoggingAspect.java
@@ -1,10 +1,7 @@
 package aop.aspects;
 
 import aop.Student;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -36,10 +33,16 @@ public class UniversityLoggingAspect {
 //                "списка студентов после работы метода getStudents()");
 //    }
 
-    @AfterThrowing(value = "execution(* getStudents())"
-        , throwing = "exception")
-    public void afterThrowingGetStudentsLoggingAdvice(Throwable exception){
-        System.out.println("afterThrowingGetStudentsLoggingAdvice: " +
-                "логируем выброс исключения " + exception);
+//    @AfterThrowing(value = "execution(* getStudents())"
+//        , throwing = "exception")
+//    public void afterThrowingGetStudentsLoggingAdvice(Throwable exception){
+//        System.out.println("afterThrowingGetStudentsLoggingAdvice: " +
+//                "логируем выброс исключения " + exception);
+//    }
+
+    @After("execution(* getStudents())")
+    public void afterGetStudentsAdvice() {
+        System.out.println("afterGetStudentsLoggingAdvice: логируем нормальное" +
+                " окончание работы метода или выброс исключения");
     }
 }
