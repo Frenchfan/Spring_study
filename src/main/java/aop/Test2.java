52144a3915b652e1c92366486c91252a8625643b
Frenchfan <sumkin.gpb@gmail.com>
2023-02-10 13:18:10 +0300

    Video10 AOP - @AfterThrowing Advice

--- src/main/java/aop/Test2.java
+++ src/main/java/aop/Test2.java
@@ -12,8 +12,13 @@ public class Test2 {
 
         University university = context.getBean("university", University.class);
         university.addStudents();
+        try{
         List<Student> students = university.getStudents();
         System.out.println(students);
+        } catch (Exception e) {
+            System.out.println("Было поймано исключение " + e);
+        }
+
 
         context.close();
 
