b5da229129e1070371e6c3b736cefdc75dfe4ed1
Frenchfan <sumkin.gpb@gmail.com>
2023-02-10 13:17:56 +0300

    Refactored Family Tree - Builder patter plus specific constructor

--- src/main/java/aop/NewFamilyBuilder.java
+++ src/main/java/aop/NewFamilyBuilder.java
@@ -14,17 +14,22 @@ public class NewFamilyBuilder {
         Person Masha = new FamilyBuilder("Маша", false).build();
         Person Kolya = new FamilyBuilder("Коля", true)
                 .setMother(Masha).setFather(Vasya).build();
-        Person Alevtina = new FamilyBuilder("Алевтина", false)
+        Person Alevtina = new Person.FamilyBuilder("Алевтина", false)
                 .setMother(Masha).setFather(Vasya).setSisterOrBrother(Kolya).build();
-        Person Vasilisa = new FamilyBuilder("Василиса Петровна", false)
+        Person Vasilisa = new Person.FamilyBuilder("Василиса Петровна", false)
                 .setChild(Vasya).build();
-        Person Nikolay = new FamilyBuilder("Николай Иванович", true)
+        Person Nikolay = new Person.FamilyBuilder("Николай Иванович", true)
                 .setChild(Vasya).build();
-        Person Olga = new FamilyBuilder("Ольга Бузова", false)
+        Person Olga = new Person.FamilyBuilder("Ольга Бузова", false)
                 .setChild(Masha).build();
-        Person Timur = new FamilyBuilder("Тимур Батрутдинов", true)
+        Person Timur = new Person.FamilyBuilder("Тимур Батрутдинов", true)
                 .setChild(Masha).build();
-        Person Filipp = new FamilyBuilder("Филипп Киркоров", true)
+        Person Filipp = new Person.FamilyBuilder("Филипп Киркоров", true)
                 .setChild(Nikolay).build();
+        Person WonderTea = new Person.FamilyBuilder("Чудо Чай", true).setMother(Alevtina).build();
+        Person Korzhik = new Person.FamilyBuilder("Коржик", true).setMother(Alevtina)
+                .setSisterOrBrother(WonderTea).build();
+        Person iRobloxQueen = new Person.FamilyBuilder("iRobloxQueen", false).setMother(Alevtina)
+                .setSisterOrBrother(Korzhik).build();
     }
 }
